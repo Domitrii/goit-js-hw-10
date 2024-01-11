5d4acf62b232c4d4157fa1fe33fed6a4be815e6c
Domitrii <126395589+Domitrii@users.noreply.github.com>
2024-01-11 16:40:24 +0100

    first-step

---
+++ src/js/1-timer.js
@@ -0,0 +1,8 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+const inputPlace = document.querySelector("#datetime-picker")
+const timerContent = document.querySelector()
+
+console.log(inputPlace.textContent)
+  
