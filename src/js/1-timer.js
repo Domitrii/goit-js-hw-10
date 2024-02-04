b74986620711ad058615cb5f38821dcc06a57dff
Domitrii <126395589+Domitrii@users.noreply.github.com>
2024-02-04 13:53:59 +0100

    finall

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,55 +1,82 @@
 import flatpickr from "flatpickr";
+import iziToast from "izitoast";
 import "flatpickr/dist/flatpickr.min.css";
+import "izitoast/dist/css/iziToast.min.css";
 
-const inputPlace = document.querySelector("#datetime-picker");
-const startBtn = document.querySelector("[data-start]");
-const modal = document.querySelector(".visible-open-block");
+let userSelectedDate;  
 
-startBtn.disable = true;
-
-let userSelectedDate;
-let a; 
-
-const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-        const selectedDate = selectedDates[0];
-        if (selectedDate.getTime() < Date.now()) {
-            modal.style.display = 'flex';
-            setTimeout(() => {
-                modal.style.display = 'none'; 
-            }, 1000);
-        }
-    },
-};
-
-flatpickr("#datetime-picker", options);
-
-function convertMs(ms) {
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-
-    const days = Math.floor(ms / day);
-    const hours = Math.floor((ms % day) / hour);
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-    return { days, hours, minutes, seconds };
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
 }
 
-startBtn.addEventListener("click", () => {
-    const selectedDate = flatpickr("#datetime-picker").selectedDates[0];
+document.addEventListener('DOMContentLoaded', function () {
+  startButton.disabled = true;  
+});
 
-    if (selectedDate) {
-        a = convertMs(selectedDate.getTime() - Date.now());
-        console.log(a);
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+const datetimePicker = document.getElementById('datetime-picker');
+const startButton = document.querySelector('[data-start]');
+const daysElement = document.querySelector('[data-days]');
+const hoursElement = document.querySelector('[data-hours]');
+const minutesElement = document.querySelector('[data-minutes]');
+const secondsElement = document.querySelector('[data-seconds]');
+
+flatpickr(datetimePicker, {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    userSelectedDate = selectedDates[0];
+    const currentDate = new Date();
+
+    if (userSelectedDate <= currentDate) {
+      iziToast.error({
+        message: 'Please choose a date in the future',
+      });
+      startButton.disabled = true;
     } else {
-        console.log("No date selected.");
+      startButton.disabled = false;
     }
-    
+  },
+});
+
+startButton.addEventListener('click', () => {
+  startButton.disabled = true;
+  datetimePicker.disabled = true;
+
+
+  const timerInterval = setInterval(() => {
+    const currentDate = new Date();
+    const timeDifference = userSelectedDate - currentDate;
+
+    if (timeDifference <= 0) {
+      clearInterval(timerInterval);
+      iziToast.success({
+        title: 'Success',
+        message: 'Countdown completed!',
+      });
+      return;
+    }
+
+    const { days, hours, minutes, seconds } = convertMs(timeDifference);
+
+    daysElement.textContent = addLeadingZero(days);
+    hoursElement.textContent = addLeadingZero(hours);
+    minutesElement.textContent = addLeadingZero(minutes);
+    secondsElement.textContent = addLeadingZero(seconds);
+  }, 1000);
 });
---
+++ src/js/2-snackbar.js
@@ -0,0 +1,37 @@
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const delay = Number(form.elements.delay.value);
+  const inputState = form.elements.state.value;
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (inputState === 'fulfilled') {
+        resolve(`Fulfilled promise in ${delay}ms`);
+      } else {
+        reject(`Rejected promise in ${delay}ms`);
+      }
+    }, delay);
+  }); 
+
+  promise
+    .then((message) => {
+      iziToast.success({
+        title: 'Success',
+        message: `✅ ${message}`,
+      });
+    })
+    .catch((message) => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ ${message}`,
+      });
+    });
+}
